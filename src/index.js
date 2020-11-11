4e2de2c2943c268c3a5c719102f158988ed78b57
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-11-11 09:18:40 +0530

    erfde

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const http = require("http");
 http
   .createServer(function (req, res) {
     if (req.url === "/welcome") {
-      res.writeHead(200);
+      res.writeHead(200, { "Content-type": "text/plain" });
       res.end("Welcome to Dominos!");
     } else if (req.url === "/contact") {
-      res.writeHead(200);
+      res.writeHead(200, { "Content-type": "application/json" });
       res.end(`{
           phone: '18602100000',
           email: 'guestcaredominos@jublfood.com'
