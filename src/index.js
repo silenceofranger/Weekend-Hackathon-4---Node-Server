d6e0efaa0eda79de7bd6145d3d4a871d9cbd8127
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-11-11 09:09:01 +0530

    ewdfw

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    if (req.url === "/welcome") {
+    if (req.url == "/welcome") {
       res.writeHead(200);
       res.end("Welcome to Dominos!");
-    } else if (req.url === "/contact") {
+    } else if (req.url == "/contact") {
       res.writeHead(200);
       res.end(`{
           phone: '18602100000',
