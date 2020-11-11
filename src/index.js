f844aad29ae341019c9fe33de60b44388ec24237
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-11-11 09:07:49 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const http = require("http");
 
-http.createServer(function (req, res) {
-    if(req.url === '/welcome')
-    {
-        res.write("Welcome to Dominos!");
-        res.end();
+http
+  .createServer(function (req, res) {
+    if (req.url === "/welcome") {
+      res.writeHead(200);
+      res.end("Welcome to Dominos!");
+    } else if (req.url === "/contact") {
+      res.writeHead(200);
+      res.end(`{
+          phone: '18602100000',
+          email: 'guestcaredominos@jublfood.com'
+          }`);
+    } else {
+      res.writeHead(404);
+      res.end();
     }
-    else if(req.url === '/contact')
-    {
-        res.write("{phone: '18602100000', email: 'guestcaredominos@jublfood.com'}")
-        res.end();
-    }
-    else
-    {
-        res.writeHead(404);
-        res.end();
-    }
-}).listen(8081);
+  })
+  .listen(8081);
